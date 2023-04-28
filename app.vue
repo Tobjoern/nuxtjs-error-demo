ec4e67bbce1555fce45389300691068590a13d1f
Tobjoern <Tobi11111@gmx.de>
2023-04-28 11:14:00 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>App</h1>
+    <Comp></Comp>
+  </div>
+</template>
+
+<script>
+import Comp from "./src/Comp.vue";
+import FromGroup from 'zod'
+
+export default {
+  name: "App",
+  components: {
+    // Comp
+    Comp: Comp
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+  mounted() {},
+  created() {},
+  destroyed() {},
+  updated() {},
+  beforeDestroy() {},
+};
+</script>
+
+<style>
+</style>
---
+++ src/Comp.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Comp</h1>
+  </div>
+</template>
+
+<script>
+import FromGroup from 'y'
+
+export default {
+  name: "Comp",
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+  mounted() {},
+  created() {},
+  destroyed() {},
+  updated() {},
+  beforeDestroy() {},
+};
+</script>
+
+<style>
+</style>
